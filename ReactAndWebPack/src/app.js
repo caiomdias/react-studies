ada48b5e201ce6684cc39f214513db7896ed773d
caio <caiomdias_@hotmail.com>
2018-01-24 22:48:38 -0200

    adiciona projeto ReactAndWebPack

---
+++ ReactAndWebPack/src/app.js
@@ -0,0 +1,14 @@
+'use strict'
+
+import React from 'react'
+var createReactClass = require('create-react-class')
+
+export var Title = createReactClass({
+    render: () => {
+        return <h1>ola fucking mundo</h1>
+    }
+})
+
+export default {
+     Title
+}
---
+++ ReactAndWebPack/src/index.js
@@ -0,0 +1,12 @@
+'use strict'
+'use strict'
+
+import React from 'react' 
+import { render } from 'react-dom' 
+import { Title } from './app'
+
+render (
+    <Title />,
+    document.getElementById('app')
+    // document.querySelector('[data-js="app"]')
+)
